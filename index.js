7a3a4a757d6178715a1b65f806d827127bd59610
CARDEER <cardeer123_1@hotmail.com>
2020-07-22 18:57:24 +0700

    first commit

--- index.js
+++ index.js
@@ -10,33 +10,52 @@ const options = {
 const https = require('https').createServer(options, app)
 const io = require('socket.io')(https)
 
+const port = 3000
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public'))
 
-const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/pages/index.html')
 })
+app.get('/multiple', (req, res) => {
+  res.sendFile(__dirname + '/pages/multiple.html')
+})
+
+var hostId = null
 
 io.on('connect', (socket) => {
-  // RTCPeerConnection
-  socket.on('peerDesc', desc => {
-    socket.broadcast.emit('peerDesc', desc)
+  socket.on('hostReady', () => {
+    hostId = socket.id
+    console.log(new Date().toLocaleString() + ' host ready ' + hostId)
   })
 
-  socket.on('peerDescAnswer', desc => {
-    socket.broadcast.emit('peerDescAnswer', desc)
+  socket.on('getSocketId', () => {
+    io.sockets.to(socket.id).emit('returnSocketId', socket.id, hostId)
+  })
+
+  socket.on('requestStream', () => {
+    console.log('request from ' + socket.id)
+    io.sockets.to(hostId).emit('requestStream', socket.id)
   })
 
   socket.on('iceCandidate', candidate => {
-    socket.broadcast.emit('iceCandidate', candidate)
+    socket.broadcast.emit('iceCandidate', socket.id, candidate)
+  })
+
+  socket.on('peer', (id, desc) => {
+    io.sockets.to(id).emit('peer', desc)
+  })
+
+  socket.on('peerAnswer', desc => {
+    io.sockets.to(hostId).emit('peerAnswer', socket.id, desc)
   })
 })
 
 https.listen(port, () => {
-  console.log('started server at port: 3000')
+  console.log('started server at port: ' + port)
 })
